24a443ed89eafbc0323b48a13959ae32a271f514
zhangyunming1990@gmail.com <zhangyunming1990@gmail.com>
2015-09-18 13:42:03 -0400

    updating to include access to ingoing edges

--- apps/ALS.C
+++ apps/ALS.C
@@ -1,8 +1,12 @@
 #define WEIGHTED 1
 #include "ligra.h"
 
+template <class vertex>
 struct ALS_Vertex_F {
-  ALS_Vertex_F() {};
+  vertex* V;
+
+  ALS_Vertex_F(vertex* _V) :
+    V(_V){};
   
   // inline bool update(uintE s, uintE d, intE rating){
   //   cout << "s: " << s << " d: " << d << " r: " << rating << endl;
@@ -15,6 +19,12 @@ struct ALS_Vertex_F {
 
   inline bool operator() (uintE i){
     cout << "vertex: " << i << endl;
+    uintE d = V[i].getInDegree();
+    for (uintE j = 0; j < d; j++){
+      uintE ngh = V[i].getInNeighbor(j);
+      intE rating = V[i].getInWeight(j);
+      cout << "edge src: " << i << " dst: " << ngh << " rating: " << rating << endl; 
+    }
   }
 };
   
@@ -25,6 +35,6 @@ void Compute(graph<vertex>& GA, commandLine P) {
   bool* frontier = newA(bool,n);
   {parallel_for(long i=0;i<n;i++) frontier[i] = 1;}
   vertexSubset Frontier(n,n,frontier);
-  vertexMap(Frontier, ALS_Vertex_F());
+  vertexMap(Frontier, ALS_Vertex_F<vertex>(GA.V));
   Frontier.del();
 }
