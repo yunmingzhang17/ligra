01ff9a6713576d9e4e1a0ebaefab051ce7b9d266
zhangyunming1990@gmail.com <zhangyunming1990@gmail.com>
2015-09-22 21:18:08 -0400

    adding the solver operation in ALS

--- apps/ALS.C
+++ apps/ALS.C
@@ -2,31 +2,53 @@
 #include "ligra.h"
 #include "eigen_wrapper.hpp"
 
+int D = 20; //number of latent factors
+double lambda = 0.065;
+
+
+struct vertex_data {
+  vec pvec;
+
+  vertex_data() {
+    pvec = zeros(D);
+  }
+  void set_val(int index, float val){
+    pvec[index] = val;
+  }
+  float get_val(int index){
+    return pvec[index];
+  }
+};
+
 template <class vertex>
 struct ALS_Vertex_F {
   vertex* V;
-  vector<float> * latent_factors_inmem;
+  //vector<float> * latent_factors_inmem;
+  std::vector<vertex_data> latent_factors_inmem;
 
   ALS_Vertex_F(vertex* _V) :
     V(_V){};
-  
-  // inline bool update(uintE s, uintE d, intE rating){
-  //   cout << "s: " << s << " d: " << d << " r: " << rating << endl;
-  // }
-
-  // inline bool updateAtomic(uintE s, uintE d, intE rating){
-  //   cout << "s: " << s << " d: " << d << " r: " << rating << endl;
-  // }
-  // inline bool cond (uintE d) { return cond_true(d);}
-
+ 
   inline bool operator() (uintE i){
     cout << "vertex: " << i << endl;
+    vertex_data & vdata = latent_factors_inmem[i];
+    mat XtX = mat::Zero(D, D); 
+    vec Xty = vec::Zero(D);
+
+
     uintE d = V[i].getInDegree();
     for (uintE j = 0; j < d; j++){
       uintE ngh = V[i].getInNeighbor(j);
       intE rating = V[i].getInWeight(j);
       cout << "edge src: " << i << " dst: " << ngh << " rating: " << rating << endl; 
     }
+    
+    double regularization = lambda;
+    //if (regnormal)
+    //regularization *= vertex.num_edges();
+    for(int i=0; i < D; i++) XtX(i,i) += regularization;
+    vdata.pvec = XtX.selfadjointView<Eigen::Upper>().ldlt().solve(Xty);
+
   }
 };
   
